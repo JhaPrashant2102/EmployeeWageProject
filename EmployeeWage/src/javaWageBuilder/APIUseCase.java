716cb8b921e25769042c8d2b329f50b4d337a313
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-09-27 14:44:23 +0530

    UC13 added

--- EmployeeWage/src/javaWageBuilder/APIUseCase.java
+++ EmployeeWage/src/javaWageBuilder/APIUseCase.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * API Application
  */
 package javaWageBuilder;
 
@@ -12,4 +12,5 @@ public interface APIUseCase {
 	//declaring some function to be used by builder class
 	public void addCompanyEmpWage(String company, int empRateHour, int maxWorkingDays, int maxWorkingHours);
 	public void calculateWage();
+	
 }
--- EmployeeWage/src/javaWageBuilder/EmpWageBuilder.java
+++ EmployeeWage/src/javaWageBuilder/EmpWageBuilder.java
@@ -3,13 +3,13 @@
  */
 package javaWageBuilder;
 
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * @author prashant
  *
  */
-public class EmpWageBuilder implements APIUSeCase {
+public class EmpWageBuilder implements APIUseCase {
 	public static final int isFullTime = 1;
 	public static final int isPartTime = 2;
 	
@@ -17,13 +17,50 @@ public class EmpWageBuilder implements APIUSeCase {
 	private LinkedList<CompanyEmpWage> wageList;
 	
 	public EmpWageBuilder() {
-		wageList = new LinkedList<CompanyEmpWage>();
+		wageList = new LinkedList<>();
 	}
 	
 	// overriding addCompanyEmpWage
 	public void addCompanyEmpWage(String company, int empRateHour, int maxWorkingDays, int maxWorkingHours) {
 		CompanyEmpWage object1 = new CompanyEmpWage(company,empRateHour,maxWorkingDays,maxWorkingHours);
 		wageList.add(object1);
+	}
+	public void calculateWage() {
+		for(int i = 0; i<wageList.size();i++) {
+			CompanyEmpWage object1 =  wageList.get(i);
+			object1.setTotalPayOfEmployee(this.calculateWage(object1));
+			System.out.println(object1);
+		}
+	}
+	
+	//Calculate wage with return type
+	public int calculateWage(CompanyEmpWage object1) {
+		int empCheck, dailyHour=0,totalEmpHours=0,totalWorkingDays=0;
+		while(totalEmpHours<=object1.maxWorkingHours&&totalWorkingDays<=object1.maxWorkingDays) {
+			empCheck = new Random().nextInt(3);
+			totalWorkingDays++;
+			switch(empCheck) {
+			case isFullTime:
+				dailyHour = 8;
+				break;
+			case isPartTime:
+				dailyHour = 4;
+				break;
+			default:
+				dailyHour = 0;
+				break;
+			}
+			totalEmpHours+=dailyHour;
+			object1.dailyWageList.add(dailyHour*object1.empRateHour);
+		}
+		return totalEmpHours*object1.empRateHour;
+		
+	}
+	public static void main(String args[]) {
+		EmpWageBuilder master = new EmpWageBuilder();
+		master.addCompanyEmpWage("Reliance",10, 20, 100);
+		master.addCompanyEmpWage("Jio", 10, 22, 120);
+		master.calculateWage();
 	}
 	
 }
